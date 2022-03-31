a867e385e2a5bd9ba346f0902dd1f00c5d79d5a7
DamarisTeoc <teoc.918@gmail.com>
2022-03-31 16:33:02 +0200

    migration factory seeder model & controller of tournament
    Co-authored-by: Sergiaparicio <Sergiaparicio@users.noreply.github.com>

---
+++ database/factories/TournamentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tournament>
+ */
+class TournamentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title= $this->faker->words(3, true);
+        $slug= strtolower(str_replace(' ', '-', $title));
+        return [
+            'title'=>$title,
+            'slug'=>$slug,
+            'description'=>$this->faker->sentence,
+            'award'=>$this->faker->numberBetween(50, 10000),
+            'date'=>$this->faker->dateTime,
+            'plataform'=>$this->faker->word,
+        ];
+    }
+}
