a2615d61944d45c36f88387eac45c5bb937096a7
Ales <aleswebgit@gmail.com>
2022-04-19 15:57:00 +0200

    add input component

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -5,26 +5,15 @@
   @method('PATCH')
   @csrf
 
-  <div class="relative pt-8 text-white bg-dark">
-    <input type="text" id="name" name="name" class="absolute bottom-0 z-10 w-full px-2 py-1 border-4 border-white peer h-9 bg-dark focus:border-secundary focus:outline-none" value="{{ $category->name }}" />
-    <label for="name" class="absolute z-50 px-1 font-semibold uppercase left-2 bg-dark bottom-6 peer-focus:text-secundary">
-      Nombre: *
-    </label>
-  </div>
-
-  <div class="relative pt-8 text-white bg-dark">
-    <input type="text" id="slug" name="slug" class="absolute bottom-0 z-10 w-full px-2 py-1 border-4 border-white peer h-9 bg-dark focus:border-secundary focus:outline-none" value="{{ $category->slug }}" />
-    <label for="slug" class="absolute z-50 px-1 font-semibold uppercase left-2 bg-dark bottom-6 peer-focus:text-secundary">
-      Slug: *
-    </label>
-  </div>
-
-  <div class="relative pt-8 text-white bg-dark">
-    <input type="text" id="image" name="image" class="absolute bottom-0 z-10 w-full px-2 py-1 border-4 border-white peer h-9 bg-dark focus:border-secundary focus:outline-none" value="{{ $category->image }}" />
-    <label for="image" class="absolute z-50 px-1 font-semibold uppercase left-2 bg-dark bottom-6 peer-focus:text-secundary">
-      Url de imagen: *
-    </label>
-  </div>
+  <x-input name="name" :value="$category->name">
+    Nombre:
+  </x-input>
+  <x-input name="slug" :value="$category->slug">
+    Slug:
+  </x-input>
+  <x-input name="image" :value="$category->image">
+    Url de imagen:
+  </x-input>
 
   <div>
     <button type="submit" class="px-4 py-2 font-bold text-white bg-dark hover:bg-secundary">
