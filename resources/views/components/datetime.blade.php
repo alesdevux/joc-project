cbede412bfc248a6512b674268a11401e8478025
Scarlet Rosa <eusouscarlett@gmail.com>
2022-04-20 14:10:53 +0200

    component

--- resources/views/components/datetime.blade.php
+++ resources/views/components/datetime.blade.php
@@ -5,10 +5,10 @@
 
 
 
-<div class=" flex justify-center items-center w-10 h-10 bg-terciary  text-center" >
-<p class="font-semibold w-5 h-6 ">{{ $slot }}</p>
+<div class=" flex justify-center items-center w-10 h-10 bg-terciary text-center" date=" {{ $date }} ">
+<p class="font-semibold w-5 h-6 ">{{ $date }}</p>
 </div>
 
-<div class= "flex justify-center items-center w-20 h-10 bg-secundary font-pop h2 text-center">
-<p class="font-semibold p-6">18:30</p>
+<div class= "flex justify-center items-center w-20 h-10 bg-secundary font-pop h2 text-center time=" {{ $time }} ">
+<p class="font-semibold p-6">{{ $time }}</p>
 </div>  
