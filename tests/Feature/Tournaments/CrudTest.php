f7730c4005a0d67bf87cfda8b1172f131af64e3d
Ales <aleswebgit@gmail.com>
2022-04-22 09:23:10 +0200

    prepare test categories have list tournaments
    Co-authored-by: Scarlet Rosa <skyrosa@users.noreply.github.com>
    Co-authored-by: DamarisTeoc <DamarisTeoc@users.noreply.github.com>

--- tests/Feature/Tournaments/CrudTest.php
+++ tests/Feature/Tournaments/CrudTest.php
@@ -9,106 +9,117 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CrudTest extends TestCase
-{
-    use RefreshDatabase;
+class CrudTest extends TestCase {
+  use RefreshDatabase;
 
-    public function test_admin_can_be_delete_tournament()
-    {
-        $this->withExceptionHandling();
+  public function test_list_tournament_appear_in_categories() {
+    $this->withExceptionHandling();
+    Tournament::all();
 
-        $userAdmin = User::factory()->create(['isAdmin' => true]);
-        $this->actingAs($userAdmin);
+    $category = Category::factory()->create();
+    $tournament = Tournament::factory()->create([
+      'category_id' => $category->id,
+    ]);
 
-        $category = Category::factory()->create();
-        $tournament = Tournament::factory()->create([
-            'category_id' => $category->id,
-        ]);
+    $response = $this->get(route('categories.show', $category->id));
+    $response->assertStatus(200)->assertViewIs('categories.show');
+  }
+  //->assertSee($tournament->title); --- no funciona pues falta tournament en la vista show de categories
 
-        $response = $this->delete(route('tournaments.delete', $tournament->id));
-        $this->assertCount(0, Tournament::all());
-    }
+  public function test_admin_can_be_delete_tournament() {
+    $this->withExceptionHandling();
 
-    public function test_user_can_not_be_delete_tournament()
-    {
-        $this->withExceptionHandling();
+    $userAdmin = User::factory()->create(['isAdmin' => true]);
+    $this->actingAs($userAdmin);
 
-        $user1 = User::factory()->create();
-        $this->actingAs($user1);
+    $category = Category::factory()->create();
+    $tournament = Tournament::factory()->create([
+      'category_id' => $category->id,
+    ]);
 
-        $category = Category::factory()->create();
-        $tournament = Tournament::factory()->create([
-            'category_id' => $category->id,
-        ]);
+    $response = $this->delete(route('tournaments.delete', $tournament->id));
+    $this->assertCount(0, Tournament::all());
+  }
 
-        $response = $this->delete(route('tournaments.delete', $tournament->id));
-        $this->assertCount(1, Tournament::all());
-    }
+  public function test_user_can_not_be_delete_tournament() {
+    $this->withExceptionHandling();
 
-    public function test_tournament_can_be_update() {
-        $this->withExceptionHandling();
+    $user1 = User::factory()->create();
+    $this->actingAs($user1);
 
-        $userAdmin = User::factory()->create(['isAdmin' => true]);
-        $this->actingAs($userAdmin);
+    $category = Category::factory()->create();
+    $tournament = Tournament::factory()->create([
+      'category_id' => $category->id,
+    ]);
 
-        $category = Category::factory()->create();
-        $tournament = Tournament::factory()->create([
-            'category_id' => $category->id,
-        ]);
+    $response = $this->delete(route('tournaments.delete', $tournament->id));
+    $this->assertCount(1, Tournament::all());
+  }
 
-        $response = $this->patch(route('tournaments.update', $tournament->id), [
-            'title' => 'new title',
-            'description' => 'new description',
-            'award' => 'new award',
-            'date' => '2020-04-01',
-            'plataform' => 'new plataform',
-        ]);
+  public function test_tournament_can_be_update() {
+    $this->withExceptionHandling();
 
-        $this->assertEquals('new title', Tournament::first()->title);       
-        $this->assertCount(1, Tournament::all());
-    }
+    $userAdmin = User::factory()->create(['isAdmin' => true]);
+    $this->actingAs($userAdmin);
 
-    public function test_view_edit_form() {
-        $this->withExceptionHandling();
+    $category = Category::factory()->create();
+    $tournament = Tournament::factory()->create([
+      'category_id' => $category->id,
+    ]);
 
-        $userAdmin = User::factory()->create(['isAdmin' => true]);
-        $this->actingAs($userAdmin);
+    $response = $this->patch(route('tournaments.update', $tournament->id), [
+      'title' => 'new title',
+      'description' => 'new description',
+      'award' => 'new award',
+      'date' => '2020-04-01',
+      'plataform' => 'new plataform',
+    ]);
 
-        $category = Category::factory()->create();
-        $tournament = Tournament::factory()->create([
-            'category_id' => $category->id,
-        ]);
+    $this->assertEquals('new title', Tournament::first()->title);
+    $this->assertCount(1, Tournament::all());
+  }
 
-        $response = $this->get(route('tournaments.edit', $tournament->id));
-        $response->assertStatus(200)->assertViewIs('tournaments.edit');
-    }
+  public function test_view_edit_form() {
+    $this->withExceptionHandling();
 
-    public function test_create_tournament() {
-        $this->withExceptionHandling();
+    $userAdmin = User::factory()->create(['isAdmin' => true]);
+    $this->actingAs($userAdmin);
 
-        $userAdmin = User::factory()->create(['isAdmin' => true]);
-        $this->actingAs($userAdmin);
+    $category = Category::factory()->create();
+    $tournament = Tournament::factory()->create([
+      'category_id' => $category->id,
+    ]);
 
-        $category = Category::factory()->create();
-        $tournament = Tournament::factory()->create([
-          'title' => 'Hola',
-          'description' => 'holiiis',
-          'award' => 'sasasa',
-          'date' => '2030-04-01',
-          'plataform' => 'new platsdaaform',
-          'category_id' => $category->id, 
-        ]);
+    $response = $this->get(route('tournaments.edit', $tournament->id));
+    $response->assertStatus(200)->assertViewIs('tournaments.edit');
+  }
 
-        $response = $this->post(route('tournaments.store', $tournament));
+  public function test_create_tournament() {
+    $this->withExceptionHandling();
 
-        $this->assertCount(1, Tournament::all());
-        $this->assertDatabaseHas('tournaments', [
-          'title' => 'Hola',
-          'description' => 'holiiis',
-          'award' => 'sasasa',
-          'date' => '2030-04-01',
-          'plataform' => 'new platsdaaform',
-          'category_id' => $category->id,
-        ]);
-    }
+    $userAdmin = User::factory()->create(['isAdmin' => true]);
+    $this->actingAs($userAdmin);
+
+    $category = Category::factory()->create();
+    $tournament = Tournament::factory()->create([
+      'title' => 'Hola',
+      'description' => 'holiiis',
+      'award' => 'sasasa',
+      'date' => '2030-04-01',
+      'plataform' => 'new platsdaaform',
+      'category_id' => $category->id,
+    ]);
+
+    $response = $this->post(route('tournaments.store', $tournament));
+
+    $this->assertCount(1, Tournament::all());
+    $this->assertDatabaseHas('tournaments', [
+      'title' => 'Hola',
+      'description' => 'holiiis',
+      'award' => 'sasasa',
+      'date' => '2030-04-01',
+      'plataform' => 'new platsdaaform',
+      'category_id' => $category->id,
+    ]);
+  }
 }
