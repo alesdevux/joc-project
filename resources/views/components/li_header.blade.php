248c17cde0ca708b2b3f90311f0198aeecc24cf2
Sergi Aparicio <sergitus2525@gmail.com>
2022-04-25 14:48:47 +0200

    li for header component

---
+++ resources/views/components/li_header.blade.php
@@ -0,0 +1,10 @@
+@props([
+    'route',
+    ])
+
+
+<li class=" uppercase w-72 ">
+    <a href="{{$route}}" class=" block w-full text-right px-6 py-2 font-semibold text-principal font-pop duration-300 hover:text-dark hover:bg-principal hover:cursor-pointer">
+        {{$slot}}
+    </a>
+</li>
