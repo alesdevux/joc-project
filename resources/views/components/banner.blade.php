ef4107d60750e84dce28ad2547b7973433b9b96e
Guillermo Labrador <glabrador970@gmail.com>
2022-04-26 15:07:22 +0200

    fix banner

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -13,8 +13,8 @@
     <p class="text-2xl font-neue group-hover:pb-1">02:59:48</p>
   </div>
   <p class="absolute z-50 w-full px-4 text-center destacar -bottom-12 group-hover:bottom-2">Ultima oportunidad para apuntarte</p>
-  <div class="absolute z-20 flex justify-between w-full h-16 px-6 py-2 scale-105 bg-secundary -bottom-4 -rotate-2 group-hover:bottom-4"></div>
-  <div class="absolute z-30 w-full h-12 px-6 scale-105 bg-principal -bottom-14 rotate-1 group-hover:-bottom-4"></div>
+  <div class="absolute z-20 flex justify-between w-full h-16 px-6 py-2 scale-125 bg-secundary -bottom-4 -rotate-2 group-hover:bottom-4 md:-rotate-1"></div>
+  <div class="absolute z-30 w-full h-12 px-6 scale-105 bg-principal -bottom-14 rotate-1 group-hover:-bottom-4 md:-bottom-16"></div>
   <div class="absolute top-0 z-10 w-full h-full bg-black/20 group-hover:bg-black/25"></div>
   <img src={{$img}} alt="" class="absolute z-0 w-full bg-cover bg-center ">
 </div>
