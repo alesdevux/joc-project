f2efe72ad75b4c9e3cb59a6e7aa543ad26693cd9
Scarlet Rosa <eusouscarlett@gmail.com>
2022-04-26 12:19:44 +0200

    tournaments show view
    Co-authored-by: Guillermo <Guillermo970@users.noreply.github.com>
    Co-authored-by: DamarisTeoc <DamarisTeoc@users.noreply.github.com>
    Co-authored-by: alesweb <aleswebgit@users.noreply.github.com>
    Co-authored-by: skyrosa <skyrosa@users.noreply.github.com>

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tournament;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TournamentController extends Controller {
     /**
